7b3837cbb4cf8247e6208db272290f5a755f56da
Bosbus <57584079+Bosbus@users.noreply.github.com>
2019-12-05 22:24:49 +0700

    Kamui

---
+++ application/modules/frontend/views/part_frontend/navigation.php
@@ -0,0 +1,37 @@
+ <div class="content-page">
+<div class="col-md-3 col-sm-3">
+  <h2>Filters</h2>
+  <!-- BEGIN FORM-->
+      <form action="#" role="form">
+        <div class="form-group">
+          <label for="career-name">Berangkat</label>
+          <input type="text" class="form-control" id="career-name">
+        </div>
+        <div class="form-group">
+          <label for="career-position">Tujuan</label>
+          <input type="text" class="form-control" id="career-name">
+        </div>
+        <div class="form-group">
+          <label for="career-resume">Kapasitas</label>
+          <input type="text" class="form-control" id="career-resume">
+        </div>
+        <button type="submit" class="btn btn-primary"><i class="icon-ok"></i> Search</button>
+      </form>
+      <br>
+  <h2>Fasilitas</h2>
+  <ul class="list-unstyled">
+    <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Pens</a>
+        <ul class="list-unstyled">
+            <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Parker Pens</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Waterman</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Ballpoint Parker Pens</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Personalised Pen Gift Sets</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Fountain Pens</a></li>
+          </ul>
+      </li>
+    <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Keyrings</a></li>
+    <li><i class="fa fa-angle-right"></i> <a href="javascript:;">Business Card Holders</a></li>
+    
+  </ul>
+</div>
+</div>
